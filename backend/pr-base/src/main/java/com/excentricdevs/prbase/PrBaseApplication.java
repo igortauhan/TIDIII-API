44b80d035af7c934a5f6512a20d9854bc97a700c
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-05-22 13:57:58 -0300

    Configure eureka client

--- backend/pr-base/src/main/java/com/excentricdevs/prbase/PrBaseApplication.java
+++ backend/pr-base/src/main/java/com/excentricdevs/prbase/PrBaseApplication.java
@@ -2,7 +2,9 @@ package com.excentricdevs.prbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class PrBaseApplication {
 
