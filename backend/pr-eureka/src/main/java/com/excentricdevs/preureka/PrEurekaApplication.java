f22f153fcd00578a6534d5da90c1491536e69391
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-05-22 13:30:18 -0300

    Configure eureka server

--- backend/pr-eureka/src/main/java/com/excentricdevs/preureka/PrEurekaApplication.java
+++ backend/pr-eureka/src/main/java/com/excentricdevs/preureka/PrEurekaApplication.java
@@ -2,7 +2,9 @@ package com.excentricdevs.preureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class PrEurekaApplication {
 
